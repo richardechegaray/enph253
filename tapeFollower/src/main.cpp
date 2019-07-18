1e59796354b63143d72aafd44c83dc98101b32b4
Richard Echegaray <richardechegaray@outlook.com>
2019-07-18 13:41:05 -0700

    ITS WORKING

--- tapeFollower/src/main.cpp
+++ tapeFollower/src/main.cpp
@@ -1,6 +1,7 @@
 //right wheel tends to be faster when speeds are set to be equal
 #include <Arduino.h>
 #include <pid.h>
+//40 seconds till u switch
 
 #define FAR_LEFT PB12
 #define LEFT_SENSOR PB13  
@@ -37,13 +38,17 @@ float error = 0.0;
 float leftBuffer[BUFFER_SIZE];
 float rightBuffer[BUFFER_SIZE];
 
+int numberOfTurns = 0;
+
 pid p_i_d;
 
-enum state { onTrack, leftOff, rightOff, turnLeft, turnRight, white, malfunc} currentState, previousState;
+enum majorState { upRamp, collectPlushie, depositPlushie/*, stones, shutDown*/ } currentMajorState;
+enum pidState { onTrack, leftOff, rightOff, turnLeft, turnRight, white, malfunc} currentPidState, previousPidState;
 
-float capSpeed(float speed); //add for all functions
-state getState(float left, float right, float farLeft, float farRight);
+float speedCapOff(float speed); //add for all functions
+pidState getPidState(float left, float right, float farLeft, float farRight);
 void drive(float bwLeft, float fwLeft, float bwRight, float fwRight);
+void pidStateMachine();
 
 void setup() {
     Serial.begin(115200);
@@ -63,124 +68,83 @@ void setup() {
     pwm_start(RIGHT_MOTOR_FW, clockFreq, period, 0, 1); 
     pwm_start(RIGHT_MOTOR_BW, clockFreq, period, 0, 1); 
     initialTime = millis();
-    previousState = onTrack;
+    previousPidState = onTrack;
     p_i_d = pid();
 }
 
 void loop() {
   timeElapsed = (millis() - initialTime)/1000; // in seconds
 
+  if (timeElapsed < 20)
+    currentMajorState = upRamp;
+  else if (timeElapsed < 45)
+    currentMajorState = collectPlushie;
+  else 
+    currentMajorState = depositPlushie;
+
   leftValue = digitalRead(LEFT_SENSOR);
   rightValue = digitalRead(RIGHT_SENSOR);
   farLeftValue = digitalRead(FAR_LEFT);
   farRightValue = digitalRead(FAR_RIGHT);
 
-  currentState = getState(leftValue, rightValue, farLeftValue, farRightValue);
-  
-  switch ( currentState ) { //state machine
+  //Serial.println(numberOfTurns);
 
-    case onTrack : //drive straight
-      error = 0; 
-      leftSpeed = targetSpeed + p_i_d.output_pid(error);
-      rightSpeed = targetSpeed + p_i_d.output_pid(error);
-      Serial.println(error);
-      Serial.println(leftSpeed);
-      Serial.println(rightSpeed);
-      Serial.println();
-      drive(0, leftSpeed, 0, rightSpeed);
+  // Serial.print((int)farLeftValue);
+  // Serial.print((int)leftValue);
+  // Serial.print((int)rightValue);
+  // Serial.println((int)farRightValue);
+  // delay(700);
+
+  switch ( currentMajorState ) {
+    case upRamp:
+      currentPidState = getPidState(leftValue, rightValue, farLeftValue, farRightValue);
+      pidStateMachine();
+      //    if (numberOfTurns == 2) {
+      //  currentMajorState = collectPlushie;
+      //}
       break;
 
-    case leftOff : //turn right
-      error = 1; 
-      leftSpeed = targetSpeed + p_i_d.output_pid(error);
-      rightSpeed = targetSpeed + p_i_d.output_pid(-error);
-      Serial.println(error);
-      Serial.println(leftSpeed);
-      Serial.println(rightSpeed);
-      Serial.println();
-      drive(0, leftSpeed, 0, rightSpeed); //left needs to catch up, right side weaker
+    case collectPlushie:
+      currentPidState = getPidState(leftValue, rightValue, farLeftValue, farRightValue);
+      pidStateMachine();
+
+      // if (numberOfTurns == 4) {
+      //   currentMajorState = depositPlushie;
+      // }
       break;
 
-    case rightOff : //turn left
-      error = 1; 
-      leftSpeed = targetSpeed + p_i_d.output_pid(-error);
-      rightSpeed = targetSpeed + p_i_d.output_pid(error);
-      Serial.println(error);
-      Serial.println(leftSpeed);
-      Serial.println(rightSpeed);
-      Serial.println();
-      drive(0, leftSpeed, 0, rightSpeed); //left weaker, right needs to catch up 
+    case depositPlushie:
+      drive(0, 0, 0, 0);
+      //Serial.println("done");
       break;
 
-    case white : //both sensors off tape
-      error = 4;
-      if(previousState == leftOff) {  // continue to turn right
-        leftSpeed = targetSpeed + p_i_d.output_pid(error);
-        rightSpeed = targetSpeed + p_i_d.output_pid(-error);
-        drive(0, leftSpeed, 0, rightSpeed); //left needs to catch up, right side weaker
-      } else if(previousState == rightOff) {  // continue to turn left
-        leftSpeed = targetSpeed + p_i_d.output_pid(-error);
-        rightSpeed = targetSpeed + p_i_d.output_pid(error);
-        drive(0, leftSpeed, 0, rightSpeed); //left weaker, right needs to catch up
-      } else if(previousState == turnLeft){ //continue to turn left
-        leftSpeed = targetSpeed + p_i_d.output_pid(-error);
-        rightSpeed = targetSpeed + p_i_d.output_pid(error);
-        drive(0, leftSpeed, 0, rightSpeed);  
-      } else if (previousState == turnRight){ //continue to turn right
-        leftSpeed = targetSpeed + p_i_d.output_pid(error);
-        rightSpeed = targetSpeed + p_i_d.output_pid(-error);
-        drive(0, leftSpeed, 0, rightSpeed);  
-      } else {
-        drive(0, 0, 0, 0); // do nothing
-        delay(2000); //if it stops this means it went from both on, to both off....
-      }
-      Serial.println(error);
-      Serial.println(leftSpeed);
-      Serial.println(rightSpeed);
-      Serial.println();
-      break;
-    
-    case turnLeft :
-      error = 9;
-      leftSpeed = targetSpeed + p_i_d.output_pid(-error);
-      rightSpeed = targetSpeed + p_i_d.output_pid(error);
-      Serial.println(error);
-      Serial.println(leftSpeed);
-      Serial.println(rightSpeed);
-      Serial.println();
-      drive(0, leftSpeed, 0, rightSpeed); //left weaker, right needs to catch up 
-      delay(250); 
-      break;
-
-    case turnRight : //not doing yet
-      error = 9;
-      leftSpeed = targetSpeed + p_i_d.output_pid(error);
-      rightSpeed = targetSpeed + p_i_d.output_pid(-error);
-      Serial.println(error);
-      Serial.println(leftSpeed);
-      Serial.println(rightSpeed);
-      Serial.println();
-      drive(0, leftSpeed, 0, rightSpeed); //left needs to catch up, right side weaker
-      delay(250); 
-      break;
-
-    default:
-      drive(0, period/3, period/3, 0); //spin for 5 seconds
-      Serial.println("Default");
-      delay(2000);  // this should never happen...
-      break;  
-
-  }
-  if (currentState != onTrack && currentState != white)
-    previousState = currentState;
-  
+  } //state machine
 }
 
-state getState(float left, float right, float farLeft, float farRight){
-  // if (farRight == ON) //first check the branch cases to not miss any of the turns
-  //   return turnRight; //if we don't see a possible turn, then keep following the "obvious" tape path
-  // if ( farLeft == ON ) 
-  //   return turnLeft;
+//increase the first turn left delay
+pidState getPidState(float left, float right, float farLeft, float farRight){
+   //first check the branch cases to not miss any of the turns
+  if (farRight == ON) {
+    if (numberOfTurns < 2 && currentMajorState == upRamp) { //if its 0 or 1
+      numberOfTurns++;
+      if (numberOfTurns == 1) { //first turn
+        delay(300); //up ramp delay
+      }
+      return turnRight;
+    }
+  }
+
+  if ( currentMajorState == collectPlushie ) {
+    if (farLeft == ON)
+      return turnLeft; 
+  }
+  //   if ( farLeft == ON ) {
+  //   if ((numberOfTurns < 4) && (numberOfTurns > 1)) { //if its 2 or 3
+  //     numberOfTurns++;
+  //     return turnLeft;
+  //   }
+  // }
+
   if ( (left == ON) && (right == ON) )
     return onTrack;
   else if ( (right == ON) && (left == OFF) )
@@ -192,11 +156,138 @@ state getState(float left, float right, float farLeft, float farRight){
   }    
 }
 
-void drive(float bwLeft, float fwLeft, float bwRight, float fwRight) {
-  fwLeft = capSpeed(fwLeft);
-  fwRight = capSpeed(fwRight);
-  bwLeft = capSpeed(bwLeft);
-  bwRight = capSpeed(bwRight);
+void pidStateMachine() {
+  switch ( currentPidState ) { //state machine
+
+    case onTrack : //drive straight
+      error = 0; 
+      leftSpeed = targetSpeed + p_i_d.output_pid(error);
+      rightSpeed = targetSpeed + p_i_d.output_pid(error);
+      // Serial.println(error);
+      // Serial.println(leftSpeed);
+      // Serial.println(rightSpeed);
+      // Serial.println();
+      drive(0, leftSpeed, 0, rightSpeed);
+      break;
+
+    case leftOff : //turn right
+      error = 1; 
+      leftSpeed = targetSpeed + p_i_d.output_pid(error);
+      rightSpeed = targetSpeed + p_i_d.output_pid(-error);
+      // Serial.println(error);
+      // Serial.println(leftSpeed);
+      // Serial.println(rightSpeed);
+      // Serial.println();
+      drive(0, leftSpeed, 0, rightSpeed); //left needs to catch up, right side weaker
+      break;
+
+    case rightOff : //turn left
+      error = 1; 
+      leftSpeed = targetSpeed + p_i_d.output_pid(-error);
+      rightSpeed = targetSpeed + p_i_d.output_pid(error);
+      // Serial.println(error);
+      // Serial.println(leftSpeed);
+      // Serial.println(rightSpeed);
+      // Serial.println();
+      drive(0, leftSpeed, 0, rightSpeed); //left weaker, right needs to catch up 
+      break;
+
+    case white : //both sensors off tape
+      error = 4;
+      if(previousPidState == leftOff) {  // continue to turn right
+        leftSpeed = targetSpeed + p_i_d.output_pid(error);
+        rightSpeed = targetSpeed + p_i_d.output_pid(-error);
+        drive(0, leftSpeed, 0, rightSpeed); //left needs to catch up, right side weaker
+      } else if(previousPidState == rightOff) {  // continue to turn left
+        leftSpeed = targetSpeed + p_i_d.output_pid(-error);
+        rightSpeed = targetSpeed + p_i_d.output_pid(error);
+        drive(0, leftSpeed, 0, rightSpeed); //left weaker, right needs to catch up
+      } else if(previousPidState == turnLeft){ //continue to turn left
+        leftSpeed = targetSpeed + p_i_d.output_pid(-error);
+        rightSpeed = targetSpeed + p_i_d.output_pid(error);
+        drive(0, leftSpeed, 0, rightSpeed);  
+      } else if (previousPidState == turnRight){ //continue to turn right
+        leftSpeed = targetSpeed + p_i_d.output_pid(error);
+        rightSpeed = targetSpeed + p_i_d.output_pid(-error);
+        drive(0, leftSpeed, 0, rightSpeed);  
+      } else {
+        drive(0, 0, 0, 0); // do nothing
+        delay(2000); //if it stops this means it went from both on, to both off....
+      }
+      // Serial.println(error);
+      // Serial.println(leftSpeed);
+      // Serial.println(rightSpeed);
+      // Serial.println();
+      break;
+    
+    case turnLeft :
+      if (numberOfTurns == 1)
+        error = 3;
+      else
+        error = 9;
+ 
+      leftSpeed = targetSpeed + p_i_d.output_pid(-error);
+      rightSpeed = targetSpeed + p_i_d.output_pid(error);
+      // Serial.println(error);
+      // Serial.println(leftSpeed);
+      // Serial.println(rightSpeed);
+      // Serial.println();
+      drive(0, leftSpeed, 0, rightSpeed); //left weaker, right needs to catch up 
+      delay(250);  
+      if (numberOfTurns == 1) {
+        delay(750);
+      } 
+
+      break;
+
+    case turnRight : //not doing yet
+      if (numberOfTurns == 1) 
+        error = 3;
+      else
+        error = 9;      
+      
+      leftSpeed = targetSpeed + p_i_d.output_pid(error);
+      rightSpeed = targetSpeed + p_i_d.output_pid(-error);
+      // Serial.println(error);
+      // Serial.println(leftSpeed);
+      // Serial.println(rightSpeed);
+      // Serial.println();
+      drive(0, leftSpeed, 0, rightSpeed); //left needs to catch up, right side weaker
+      delay(250); 
+     if (numberOfTurns == 1) {
+       delay(750);
+     } 
+      break;
+
+    default:
+      drive(0, 0, 0, 0); //spin for 5 seconds
+      // Serial.println("Default");
+      delay(5000);  // this should never happen...
+      break;  
+  }
+
+  if (currentPidState != onTrack && currentPidState != white)
+    previousPidState = currentPidState;
+
+  // if (numberOfTurns == 1) 
+  //   previousPidState == rightOff; //continue to turn left on initial turn
+}
+
+void drive(float bwLeft, float fwLeft, float bwRight, float fwRight) { // DO NOT TRY TO RUN FW AND BW DIRECTION FOR ONE WHEEL AT A TIME
+  fwLeft = speedCapOff(fwLeft);
+  fwRight = speedCapOff(fwRight);
+
+  // if (fwRight < 0) {
+  //   bwRight = -fwRight;
+  //   fwRight = 0;
+  // }
+  // if (fwLeft < 0) {
+  //   bwLeft = -fwLeft;
+  //   fwLeft = 0;
+  // }
+
+  bwLeft = speedCapOff(bwLeft);
+  bwRight = speedCapOff(bwRight);
 
   pwm_start(LEFT_MOTOR_BW, clockFreq, period, bwLeft, 0); 
   pwm_start(LEFT_MOTOR_FW, clockFreq, period, fwLeft, 0); 
@@ -204,11 +295,11 @@ void drive(float bwLeft, float fwLeft, float bwRight, float fwRight) {
   pwm_start(RIGHT_MOTOR_FW, clockFreq, period, fwRight, 0); 
 }
 
-float capSpeed(float speed) {
+float speedCapOff(float speed) {
   if (speed>period)
     return period;
   else if (speed<0)
-  return period/10;
+  return 12*period/100;
   else
     return speed;
 }
