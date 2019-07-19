db192f5784a13b17230021b93a10ce620a0088cc
Richard Echegaray <richardechegaray@outlook.com>
2019-07-18 21:45:06 -0700

    working plushie deposit without doors moving

---
+++ tapeFollower/src/IRsensor.cpp
@@ -0,0 +1,96 @@
+#include <Arduino.h>
+#include "IRsensor.h"
+#define SIZE 100
+
+float raw_sample [SIZE];
+float waveform [2*SIZE];
+
+IRsensor::IRsensor(){
+    return;
+}
+
+IRsensor::IRsensor(int set_pin, int set_mode){
+    pin = set_pin;
+    pinMode(pin, INPUT);
+
+    sample_size = SIZE; // the amount of values to collect into array for reading
+
+    mode = set_mode;
+    if (mode == 1){
+        power = 3;
+    }
+    else if (mode == 10){
+        power = 4;
+    }
+    else {
+        Serial.println("Error: incorrect mode");
+    }
+}
+
+void IRsensor::sample(int pin){
+    for (int i = 0; i < sample_size; i++){
+        raw_sample[i] = analogRead(pin);
+    }
+}
+
+void IRsensor::corr(){
+    time_start = micros();
+    sample(pin);
+    time_end = micros();
+    duration = (time_end - time_start)/(pow(10,6));
+    time_per_sample = duration/sample_size;
+
+    // find average of samples 
+    float sum_sample = 0.0;
+    for (int i = 0; i < sample_size; i++){
+        sum_sample += raw_sample[i];
+    }
+    average = sum_sample/sample_size;
+
+    // subtract every value in the array by that to centre it
+    for (int i = 0; i < sample_size; i++){
+        raw_sample[i] = raw_sample[i] - average;
+    }
+
+    // generate mode array
+    for (int i = 0; i < (2*sample_size); i++){
+        waveform[i] = sin(2*PI*pow(10,power)*i*time_per_sample);
+    }
+    // compare sample array to the generated array
+    // correlation = IRsamples*waveform
+    float corr_values[sample_size] = {};
+    for (int m = 0; m < (sample_size); m++){
+        for (int n = 0; n < (sample_size); n++){
+            corr_values[m] += raw_sample[n]*waveform[m+n];
+        }
+    }
+
+    float highest_val = -1000.0;
+
+    for (float x : corr_values){
+        if (x > highest_val){
+            highest_val = x;
+        }
+    }
+
+    // Serial.println("raw:");
+    // for (int i = 0; i < 10; i++){
+    //     Serial.println(raw_sample[i]);
+    // }
+
+    // Serial.println("wave:");
+    // for (int i = 0; i < 10; i++){
+    //     Serial.println(waveform[i]);
+    // }
+
+    // Serial.println("corr_values:");
+    // for (int i = 0; i < 10; i++){
+    //     Serial.println(corr_values[i]);
+    // }
+
+    correlation = highest_val;
+
+    // for (int i = 0; i < (2*sample_size); i++){
+    //     correlation += corr_values[i];
+    // }
+}
--- tapeFollower/src/main.cpp
+++ tapeFollower/src/main.cpp
@@ -1,6 +1,8 @@
 //right wheel tends to be faster when speeds are set to be equal
 #include <Arduino.h>
 #include <pid.h>
+#include "IRdecision.h" 
+#include "ultrasonic.h"
 //40 seconds till u switch
 
 #define FAR_LEFT PB12
@@ -10,13 +12,18 @@
 
 #define LEFT_MOTOR_FW PB_9 
 #define LEFT_MOTOR_BW PB_8
-
 #define RIGHT_MOTOR_FW PB_6
 #define RIGHT_MOTOR_BW PB_7
 
-#define BUFFER_SIZE 5
 #define ON 1
 #define OFF 0
+
+#define LEFT_IR PA2
+#define MID_IR PA1
+#define RIGHT_IR PA0
+
+#define TRIG PB10
+#define ECHO PB11
 
 float clockFreq = 100000;
 float period = 1000;
@@ -24,31 +31,48 @@ float period = 1000;
 float initialTime;
 float timeElapsed;
 
+float spinSpeed = 22*period/100; //make sure this isnt too fast, 20 is too slow
+float targetIrSpeed = 25*period/100;
+float targetIrSpeedPlus = 30*period/100;
+float targetIrSpeedMinus = 20*period/100;
+
 float targetSpeed = 50*period/100;
 float leftSpeed = targetSpeed;
 float rightSpeed = targetSpeed;
+
+float midThreshold = 5000; //past columns
+float closeThreshold = 6000;  //quite close
+
+float midIntensity = -1;
+int highestPin = -1;
+//int detectionRange; uSonic
 
 float leftValue = 0.0;
 float rightValue = 0.0;
 float farLeftValue = 0.0;
 float farRightValue = 0.0;
 
-float error = 0.0;
-
-float leftBuffer[BUFFER_SIZE];
-float rightBuffer[BUFFER_SIZE];
+float error = 0.0;;
 
 int numberOfTurns = 0;
 
 pid p_i_d;
+ultrasonic ultra = ultrasonic(TRIG, ECHO);
+IRdecision decision = IRdecision(LEFT_IR, MID_IR, RIGHT_IR, 1); //1 kHz
 
-enum majorState { upRamp, collectPlushie, depositPlushie/*, stones, shutDown*/ } currentMajorState;
+enum majorState { upRamp, collectPlushie, depositPlushie, stones/*, shutDown*/ } currentMajorState;
 enum pidState { onTrack, leftOff, rightOff, turnLeft, turnRight, white, malfunc} currentPidState, previousPidState;
+enum irState { initialSpin, drivingFar, drivingMiddle, drivingClose, /*avoid,*/ stop} currentIrState;
+enum range { far, mid, close } currentDistance;
+
 
 float speedCapOff(float speed); //add for all functions
 pidState getPidState(float left, float right, float farLeft, float farRight);
 void drive(float bwLeft, float fwLeft, float bwRight, float fwRight);
 void pidStateMachine();
+void irStateMachine();
+void irDrive(range distance);
+
 
 void setup() {
     Serial.begin(115200);
@@ -77,7 +101,7 @@ void loop() {
 
   if (timeElapsed < 13)
     currentMajorState = upRamp;
-  else if (timeElapsed < 30)
+  else if (timeElapsed < 28)
     currentMajorState = collectPlushie;
   else 
     currentMajorState = depositPlushie;
@@ -114,8 +138,12 @@ void loop() {
       break;
 
     case depositPlushie:
-      drive(0, 0, 0, 0);
+      irStateMachine();
       //Serial.println("done");
+      break;
+
+    case stones:
+      drive(targetSpeed/2, 0, targetSpeed/2, 0);
       break;
 
   } //state machine
@@ -273,6 +301,90 @@ void pidStateMachine() {
   //   previousPidState == rightOff; //continue to turn left on initial turn
 }
 
+void irStateMachine() {
+switch ( currentIrState ) { //state machine
+
+    case initialSpin : //drive straight
+      highestPin = decision.strongest_signal();
+      midIntensity = decision.corrcenter;
+      drive(spinSpeed, 0, 0, spinSpeed); // spin CCW
+      
+      if ((highestPin == MID_IR) && (midIntensity > 100)) {
+        drive(0,0,0,0);
+        currentIrState = drivingFar;
+      }
+      break;
+
+    case drivingFar :
+      // detectionRange = 25; //ultrasonic
+      // if ((ultra.is_there_obj(detectionRange)) && currentState == driving) {
+      // currentState = avoid;  
+      // }
+      midIntensity = decision.corrcenter;
+      leftValue = digitalRead(LEFT_SENSOR);
+      rightValue = digitalRead(RIGHT_SENSOR);
+      farLeftValue = digitalRead(FAR_LEFT);
+      farRightValue = digitalRead(FAR_RIGHT);
+
+      if (midIntensity > closeThreshold) {
+        currentDistance = close; 
+        currentIrState = drivingClose;
+        irDrive(currentDistance);
+      } else if (midIntensity > midThreshold) {
+        currentDistance = mid;
+        currentIrState = drivingMiddle;
+        irDrive(currentDistance);
+      } else
+        irDrive(currentDistance);
+
+      break;  
+
+    case drivingMiddle :
+      midIntensity = decision.corrcenter;
+      leftValue = digitalRead(LEFT_SENSOR);
+      rightValue = digitalRead(RIGHT_SENSOR);
+      farLeftValue = digitalRead(FAR_LEFT);
+      farRightValue = digitalRead(FAR_RIGHT);
+     
+      if (midIntensity > closeThreshold) {
+        currentDistance = close; 
+        currentIrState = drivingClose;
+        irDrive(currentDistance);
+      } else
+        irDrive(currentDistance);
+      
+      break;
+
+    case drivingClose :
+      leftValue = digitalRead(LEFT_SENSOR);
+      rightValue = digitalRead(RIGHT_SENSOR);
+      farLeftValue = digitalRead(FAR_LEFT);
+      farRightValue = digitalRead(FAR_RIGHT);
+
+      if (leftValue || rightValue || farLeftValue || farRightValue == ON) {
+        currentIrState = stop;
+        break;
+      }
+
+      irDrive(currentDistance);
+
+      break;    
+
+    // case avoid:
+    //   drive(targetSpeed*2, 0, 0, targetSpeed*2);
+    //   delay(300);
+    //   drive(0, targetSpeed*2, 0, targetSpeed*2);
+    //   delay(600);
+    //   currentState = initialSpin;
+    //   break;
+    
+    case stop:
+      drive(0,0,0,0);
+      currentMajorState = stones;
+      break;
+ }
+}
+
 void drive(float bwLeft, float fwLeft, float bwRight, float fwRight) { // DO NOT TRY TO RUN FW AND BW DIRECTION FOR ONE WHEEL AT A TIME
   fwLeft = speedCapOff(fwLeft);
   fwRight = speedCapOff(fwRight);
@@ -295,6 +407,35 @@ void drive(float bwLeft, float fwLeft, float bwRight, float fwRight) { // DO NOT
   pwm_start(RIGHT_MOTOR_FW, clockFreq, period, fwRight, 0); 
 }
 
+void irDrive(range distance) {
+  float speed, speedPlus, speedMinus;
+  int maxVal;
+
+  if (distance == far) {
+    speed = targetSpeed;
+    speedPlus = 110*targetIrSpeedPlus/100;
+    speedMinus = 110*targetIrSpeedMinus/100;
+  } else if (distance == mid) {
+    speed = targetSpeed;
+    speedPlus = targetIrSpeedPlus;
+    speedMinus = targetIrSpeedMinus;
+  } else if (distance == close) {
+    speed = 4*targetSpeed/5;
+    speedPlus = 4*targetIrSpeedPlus/5;
+    speedMinus = 4*targetIrSpeedMinus/5;
+  } else {
+    speed = speedPlus = speedMinus = 0;
+  }
+
+  maxVal = decision.strongest_signal();
+  if (maxVal == LEFT_IR) 
+    drive(0, speedMinus, 0, speedPlus);
+  else if (maxVal == RIGHT_IR) 
+    drive(0, speedPlus, 0, speedMinus);
+  else
+    drive(0, speed, 0, speed);
+}
+
 float speedCapOff(float speed) {
   if (speed>period)
     return period;
@@ -303,3 +444,4 @@ float speedCapOff(float speed) {
   else
     return speed;
 }
+
